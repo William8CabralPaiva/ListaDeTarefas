8a3b4acf9325a8460222713ea19e3ec4de113b39
“William8Cabral” <suporte@vxm.com.br>
2025-01-22 20:45:29 -0300

    add application

---
+++ app/src/main/java/com/cabral/listadetarefas/TodoApplication.kt
@@ -0,0 +1,21 @@
+package com.cabral.listadetarefas
+
+import android.app.Application
+import com.cabral.listadetarefas.di.AppModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class TodoApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@TodoApplication)
+            modules(
+                AppModule.module
+            )
+        }
+    }
+}
